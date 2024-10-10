bd8eca43f5a8458134f20b1329e31cbee48229e2
Justin Tanner <work@jwtanner.com>
2024-10-10 15:00:55 +0700

    Removed arch check

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-set -e
 
 echo -e "Installing Rails Academy...\n"
-
-ARCH=$(uname -m)
-
-if [ "$ARCH" != "amd64" ]; then
-  echo "This script only supports amd64 systems. Your architecture is ${ARCH}."
-  exit 1
-fi
 
 echo "Updating package lists..."
 sudo apt update -y
