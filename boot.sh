a37fc4b78f4d79ef84e65bf56751fed42e88a455
Justin Tanner <work@jwtanner.com>
2024-10-10 23:08:14 +0700

    Simplified boot.sh

--- boot.sh
+++ boot.sh
@@ -4,13 +4,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Detected a Mac..."
   eval "curl -fsSL https://raw.githubusercontent.com/justintanner/rails-academy/stable/mac/install.sh | bash -i"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  if grep -q "microsoft" /proc/version; then
-    echo "Detected WSL on Windows..."
-    eval "wget -qO- https://raw.githubusercontent.com/justintanner/rails-academy/stable/ubuntu/install.sh | bash -i"
-  else
-    echo "Detected Linux..."
-    eval "wget -qO- https://raw.githubusercontent.com/justintanner/rails-academy/stable/ubuntu/install.sh | bash -i"
-  fi
+  echo "Detected Linux..."
+  eval "wget -qO- https://raw.githubusercontent.com/justintanner/rails-academy/stable/ubuntu/install.sh | bash -i"
 else
   echo "Unsupported OS: $OSTYPE"
   exit 0
