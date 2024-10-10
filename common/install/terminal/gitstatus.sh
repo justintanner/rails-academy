c46b2e5cc780c78fb14e1185f72ec40f3c8881d3
Justin Tanner <work@jwtanner.com>
2024-10-10 17:42:40 +0700

    Removing the old gitstatus dir on install

--- common/install/terminal/gitstatus.sh
+++ common/install/terminal/gitstatus.sh
@@ -1,4 +1,5 @@
 DEST="$HOME/.local/share/gitstatus"
+rm -rf $DEST
 mkdir -p $DEST
 git clone --depth=1 https://github.com/romkatv/gitstatus.git $DEST
 echo 'source ~/gitstatus/gitstatus.prompt.sh' >> ~/.bashrc
