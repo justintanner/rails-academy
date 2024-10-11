d4e19a2544dc7babf702784efa1feb823963a611
Justin Tanner <work@jwtanner.com>
2024-10-11 16:24:06 +0700

    Unified #! for better cross platform scripting and hopefully activated mise on WSL

--- mac/install.sh
+++ mac/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo -e "Installing Rails Academy on a Mac...\n"
 
--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 echo -e "Installing Rails Academy on Ubuntu...\n"
