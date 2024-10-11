5267a563d55e85eb287873e50404a60387e28d92
Justin Tanner <work@jwtanner.com>
2024-10-11 15:39:07 +0700

    Making windows symbolic link work better

--- common/install_lessons.sh
+++ common/install_lessons.sh
@@ -2,7 +2,7 @@ if [ -d /mnt/c/Users ]; then
   echo "Symlinking ralessons to the Windows home dir...."
   windows_username=$(echo $PATH | grep -oP '/mnt/c/Users/\K[^/]+')
   mkdir -p /mnt/c/Users/$windows_username/ralessons
-  ln -s /mnt/c/Users/$windows_username/ralessons ~/ralessons
+  ln -sf /mnt/c/Users/$windows_username/ralessons ~/ralessons
 else
   mkdir -p ~/ralessons
 fi
