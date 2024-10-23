a70468cb0dc22121a971b10d66aad7b5ac4f5ee8
Justin Tanner <work@jwtanner.com>
2024-10-23 15:49:28 +0700

    Forcing interactive mode

--- boot.sh
+++ boot.sh
@@ -2,10 +2,10 @@
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Detected a Mac..."
-  eval "curl -fsSL https://raw.githubusercontent.com/justintanner/rails-academy/stable/mac/install.sh | bash"
+  eval "curl -fsSL https://raw.githubusercontent.com/justintanner/rails-academy/stable/mac/install.sh | bash -i"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "Detected Linux..."
-  eval "wget -qO- https://raw.githubusercontent.com/justintanner/rails-academy/stable/ubuntu/install.sh | bash"
+  eval "wget -qO- https://raw.githubusercontent.com/justintanner/rails-academy/stable/ubuntu/install.sh | bash -i"
 else
   echo "Unsupported OS: $OSTYPE"
   exit 0
