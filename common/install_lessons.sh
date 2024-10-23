02b24000fa49f3c7db5fe5a329e70f7348c9cd84
Justin Tanner <work@jwtanner.com>
2024-10-23 15:54:51 +0700

    Forcing interactive mode part4

--- common/install_lessons.sh
+++ common/install_lessons.sh
@@ -13,3 +13,14 @@ else
   git clone https://github.com/justintanner/ra-101 ~/lessons/ra-101
 fi
 
+if [ -d ~/lessons/ra-102 ]; then
+  echo "RA-102 already exists. Skipping..."
+else
+  git clone https://github.com/justintanner/ra-102 ~/lessons/ra-102
+fi
+
+if [ -d ~/lessons/ra-103 ]; then
+  echo "RA-102 already exists. Skipping..."
+else
+  git clone https://github.com/justintanner/ra-103 ~/lessons/ra-103
+fi
