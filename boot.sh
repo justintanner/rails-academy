09596488780385b17aa75feaaba64597ff9d9539
Justin Tanner <work@jwtanner.com>
2024-10-24 11:28:35 +0700

    Trying to force interactive mode again

--- boot.sh
+++ boot.sh
@@ -1,11 +1,9 @@
-#!/usr/bin/env bash
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Detected a Mac..."
-  eval "curl -fsSL https://raw.githubusercontent.com/justintanner/rails-academy/stable/mac/install.sh | bash"
+  eval "curl -fsSL https://raw.githubusercontent.com/justintanner/rails-academy/stable/mac/install.sh | bash -i"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "Detected Linux..."
-  eval "wget -qO- https://raw.githubusercontent.com/justintanner/rails-academy/stable/ubuntu/install.sh | bash"
+  eval "wget -qO- https://raw.githubusercontent.com/justintanner/rails-academy/stable/ubuntu/install.sh | bash -i"
 else
   echo "Unsupported OS: $OSTYPE"
   exit 0
