e3c6cc83ea67dc3c271c775580eef8ac6545952b
Justin Tanner <work@jwtanner.com>
2024-10-28 15:04:19 +0700

    Better fonts installs script

--- ubuntu/install/terminal/fonts.sh
+++ ubuntu/install/terminal/fonts.sh
@@ -5,8 +5,9 @@ FILE_NAME="${FONT_NAME/ Nerd Font/}"
 cd /tmp
 wget -O "$FILE_NAME.zip" "$URL"
 unzip "$FILE_NAME.zip" -d "$FILE_NAME"
+mkdir -p ~/.loca/share/fonts
 cp "$FILE_NAME"/*."ttf" ~/.local/share/fonts
 rm -rf "$FILE_NAME.zip" "$FILE_NAME"
-fc-cache
+fc-cache -fv
 cd -
 clear
